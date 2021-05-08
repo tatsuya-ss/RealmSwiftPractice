73c51587b8980f51f42e930340497e44289e8705
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2021-05-08 10:57:37 +0900

    textField入力内容を保存・更新・取得処理を追加 \n ViewControllerからRealmSwiftを切り分けた

--- RealmSwiftPractice/ViewController.swift
+++ RealmSwiftPractice/ViewController.swift
@@ -7,41 +7,53 @@
 
 import UIKit
 
+struct MovieContent {
+    var title: String
+    var releaseDay: String
+    var overview: String
+    var review: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var releaseTextField: UITextField!
     @IBOutlet weak var overviewTextField: UITextField!
     @IBOutlet weak var reviewTextField: UITextField!
     
-    
+    let movieInfomations = MovieInfomation()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        getMovieInfomation()
+        if let movieInfo = movieInfomations.fetchMovieInfomation() {
+            titleTextField.text = movieInfo.title
+            releaseTextField.text = movieInfo.releaseDay
+            overviewTextField.text = movieInfo.overview
+            reviewTextField.text = movieInfo.review
+        } else {
+            setRealm()
+        }
     }
 
     @IBAction func saveButtonTapped(_ sender: Any) {
-        setRealm()
+        saveMovieInfomation()
+    }
+    
+    func saveMovieInfomation() {
+        let movieContent = MovieContent(title: titleTextField.text ?? "",
+                                        releaseDay: releaseTextField.text ?? "",
+                                        overview: overviewTextField.text ?? "",
+                                        review: reviewTextField.text ?? "")
+        movieInfomations.saveMovieInfomation(movieContent: movieContent)
+        print("saveしました\(movieContent.title)")
     }
     
     func setRealm() {
-        let title = titleTextField.text ?? ""
-        let release = releaseTextField.text ?? ""
-        let overview = overviewTextField.text ?? ""
-        let review = reviewTextField.text ?? ""
-        
-        let movieInfomations = MovieInfomation()
-        movieInfomations.setMovieInfomation(title: title, releaseDay: release, overview: overview, review: review)
-        print("保存しました\n\(title)\n\(release)\n\(overview)\n\(review)")
-    }
-    
-    func getMovieInfomation() {
-        let movieInfomations = MovieInfomation()
-        let movies = movieInfomations.fetchMovieInfomation()
-        
-        titleTextField.text = movies?.title
-        releaseTextField.text = movies?.releaseDay
-        overviewTextField.text = movies?.overview
-        reviewTextField.text = movies?.review
+        let movieContent = MovieContent(title: titleTextField.text ?? "",
+                                        releaseDay: releaseTextField.text ?? "",
+                                        overview: overviewTextField.text ?? "",
+                                        review: reviewTextField.text ?? "")
+        movieInfomations.setMovieInfomation(movieContent: movieContent)
+        print("初期setしました\(movieContent.title)")
     }
 }
 
